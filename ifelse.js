6c225775b52fe8c7bf19c6098aca2e5845c3f7f0
aarohikanoujia27 <roshanikanoujia99100@gmail.com>
2025-03-06 15:21:03 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,62 @@
+// function condition(gender) {
+//   debugger;
+//   if (gender == "female") {
+//     String = "aarohi";
+//   } else if (gender == "male") {
+//     debugger;
+//     String = "aman";
+//   } else {
+//     String = "pratham";
+//   }
+//   return String;
+// }
+// console.log(condition('animal'));
+
+
+// let age = 20;
+// let message;
+// debugger;
+// if (age >= 18) {
+//     message = "Adult";
+// } else {
+//     message = "Minor";
+// }
+
+// console.log(message);
+
+
+// let isMember = false;
+// let discount = isMember ? "Discount Applied" : "No Discount";
+// console.log(discount);
+
+
+
+// let age = 25;
+// let hasID = true;
+// debugger;
+// if (age >= 18 && hasID) {
+//     console.log("You can enter the club.");
+// } else {
+//     console.log("You cannot enter.");
+// }
+
+
+
+// let hasVIPPass = false;
+// let hasTicket = false;
+// debugger;
+// if ( hasTicket || hasVIPPass) {
+//     console.log("You can enter the concert.");
+// } else {
+//     console.log("You cannot enter.");
+// }
+
+
+
+let isRaining = true;
+debugger;
+if (!isRaining) {
+    console.log("Let's go outside!");
+} else {
+    console.log("Stay inside.");
+}
